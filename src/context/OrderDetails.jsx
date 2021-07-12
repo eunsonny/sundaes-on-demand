fc9f7ed48cdf890995f95df57be61dafff5a2ec9
eunsunShin <unusual0218@gmail.com>
2021-07-12 23:15:05 +0900

    Add: code using provider

---
+++ src/context/OrderDetails.jsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState, useMemo, useEffect } from "react";
+import { pricePerItem } from "../constants";
+
+const OrderDetails = createContext();
+
+// create custom hook to check whether we're inside a provider
+export function useOrderDetails() {
+  const context = useContext(OrderDetails);
+
+  if (!context) {
+    throw new Error(
+      "useOrderDetails must be used within an OrderDetailsProvider"
+    );
+  }
+
+  return context;
+}
+
+export function OrderDetailsProvider(props) {
+  const [optionCounts, setOptionCounts] = useState({
+    scoops: new Map(),
+    toppings: new Map(),
+  });
+  const [totals, setTotals] = useState({
+    scoops: 0,
+    toppings: 0,
+    grandTotal: 0,
+  })
+
+  function calculateSubtotal(orderType, optionCounts) {
+    let optionCount = 0;
+    for (const count of optionCounts[optionsType].values()) {
+      optionCount += count;
+    }
+
+    return optionCount * pricePerItem[optionType];
+  }
+
+  useEffect(() => {
+    const scoopsSubtotal = calculateSubtotal("scoops", optionsCounts);
+    const toppingSubtotal = calculateSubtotal("topping", optionCounts);
+    const grandTotal = scoopsSubtotal + toppingSubtotal;
+    setTotals({
+      scoops: scoopsSubtotal,
+      toppings: toppingSubtotal,
+      grandTotal
+    })
+  }, [optionCounts])
+
+  const value = useMemo(() => {
+    function updateItemCount(itemName, newItemCount, optionType) {
+      const newOptionCounts = { ...optionCounts }
+
+      // update option count fro this item with the new value
+      const optionCountsMap = optionCounts[optionType];
+      optionCountsMap.set(itemName, parseInt(newItemCount));
+
+      setOptionCounts(newOptionCounts);
+    }
+
+    // getter: object containing options count for scoops and toppings, subtotal and totals
+    // setter: updateOptionCount
+    return [{ ...optionCounts, totals }, updateItemCount];
+  }, [optionCounts, totals]);
+
+  return <OrderDetails.Provider value={value} {...props} />
+}
+
+export { OrderDetailsProvider, useOrderDetails }
--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import axios from "axios";
 import ScoopOption from "./ScoopOption";
-import ToppingOption from "./ToppingOption"
+import ToppingOption from "./ToppingOption";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { useOrderDetails } from "../../context/OrderDetails";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
@@ -20,21 +23,31 @@ export default function Options({ optionType }) {
       });
   }, [optionType]);
 
-  if(error) {
+  if (error) {
     return <AlertBanner />;
   }
 
-
   // TODO: replace 'null' with ToppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].tpUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <Fragment>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>{title} total: {orderDetails.totals[optionType]}</p>
+      <Row>{optionItems}</Row>;
+    </Fragment>
+  );
 }
